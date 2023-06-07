1e54600a5dc22433d46f63537925d3ca4998d354
Michael Brew <themichaelbrew@gmail.com>
2023-06-07 00:06:03 +0000

    updated checkout

--- src/components/guest/shop/cart/CheckoutSuccessful.tsx
+++ src/components/guest/shop/cart/CheckoutSuccessful.tsx
@@ -1,45 +1,75 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Button from "react-bootstrap/Button";
 import { useNavigate } from "react-router-dom";
 
+import { CheckoutService } from "services/checkout.service";
 import { CartContext } from "contexts/CartContext";
+import { Loading } from "components/sub-components/loading/Loading";
 import "./cart.css";
 
+const checkoutService = new CheckoutService();
+
 export const CheckoutSuccessful = () => {
-    const { setCartItems } = useContext(CartContext);
-    const navigate = useNavigate();
+  const { setCartItems } = useContext(CartContext);
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(true);
 
-    const emptyCart = () => {
-        const cartItems = localStorage.getItem("cartItems");
+  const verifyCheckout = async () => {
+    try {
+      setLoading(true);
+      const ref = localStorage.getItem("reference");
 
-        if (cartItems) {
-            localStorage.setItem("cartItems", JSON.stringify([]));
-            setCartItems?.([]);
+      if (ref) {
+        const response = await checkoutService.verify(ref);
+        if (response?.status === 200) {
+          localStorage.removeItem("reference");
+          setLoading(false);
         }
-    };
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-    return (
-        <div className="checkout-response-wrapper">
-            <div>
-                <div className="d-flex justify-content-center">
-                    <h3>Thank you for your purchase</h3>
-                </div>
-                <div className="my-5">
-                    An email has been sent to your account. Please check for
-                    further details.
-                </div>
-                <div className="d-flex justify-content-center">
-                    <Button
-                        variant="outline-secondary"
-                        onClick={() => {
-                            emptyCart();
-                            navigate("/shop");
-                        }}
-                    >
-                        Back To Shop
-                    </Button>
-                </div>
-            </div>
+  useEffect(() => {
+    verifyCheckout();
+  }, []);
+
+  const emptyCart = () => {
+    const cartItems = localStorage.getItem("cartItems");
+
+    if (cartItems) {
+      localStorage.setItem("cartItems", JSON.stringify([]));
+      setCartItems?.([]);
+    }
+  };
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  return (
+    <div className="checkout-response-wrapper">
+      <div>
+        <div className="d-flex justify-content-center">
+          <h3>Thank you for your purchase</h3>
         </div>
-    );
+        <div className="my-5">
+          An email has been sent to your account. Please check for further
+          details.
+        </div>
+        <div className="d-flex justify-content-center">
+          <Button
+            variant="outline-secondary"
+            onClick={() => {
+              emptyCart();
+              navigate("/shop");
+            }}
+          >
+            Back To Shop
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 };
--- src/services/checkout.service.ts
+++ src/services/checkout.service.ts
@@ -1,20 +1,31 @@
 import axiosInstance from "../utils/interceptors";
 
 export class CheckoutService {
-    public checkout = async (details: ICheckoutDetails) => {
-        console.log(details);
-        try {
-            const response = await axiosInstance.post(
-                "/api/create-checkout-session",
-                { items: details.items },
-                {
-                    headers: { "Content-Type": "application/json" },
-                }
-            );
-
-            return response;
-        } catch (error) {
-            console.log(error);
+  public checkout = async (details: ICheckoutDetails) => {
+    try {
+      const response = await axiosInstance.post(
+        "/api/checkout/initialize",
+        { items: details.items },
+        {
+          headers: { "Content-Type": "application/json" },
         }
-    };
+      );
+
+      return response;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  public verify = async (ref: string) => {
+    try {
+      const response = await axiosInstance.get(
+        `/api/checkout/verify?reference=${ref}`
+      );
+
+      return response;
+    } catch (error) {
+      console.log(error);
+    }
+  };
 }
